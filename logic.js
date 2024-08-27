66680ee20f11633e718b6e448a78bd88045e856b
ChrisVizcaino <161230962+ChrisVizcaino@users.noreply.github.com>
2024-08-27 09:22:49 -0500

    Uploading first version.
    Need to change a lot of things. Work on the project

---
+++ logic.js
@@ -0,0 +1,48 @@
+const textArea = document.querySelector(".cuadro_de_texto");
+const respuesta = document.querySelector(".Respuesta_de_texto");
+const quoteText = document.querySelector(".Respuesta_de_texto");
+let handleCopyClick = document.querySelector('.btn3');
+
+function buttonEncriptar(){
+    let textoEncriptado = encriptar(textArea.value)
+    respuesta.value = textoEncriptado
+    textArea.value = "";
+    respuesta.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado;
+}
+
+function buttonDesencriptar(){
+    let textoDesencriptado = desencriptar(textArea.value)
+    respuesta.value = textoDesencriptado
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptado
+}
+
+handleCopyClick.addEventListener('click', () => {
+
+    navigator.clipboard.writeText(`${encriptar(textArea.value)}`);
+  
+    alert(`Copied to clipboard!`);
+  });
